4fb392eb370f12681d7227968c07c5c59e74828f
j0sephh123 <j0sephh@abv.bg>
2019-02-25 11:31:07 +0200

    d

--- src/js/cms-preview-templates/Items/items.js
+++ src/js/cms-preview-templates/Items/items.js
@@ -4,7 +4,8 @@ export default class ItemsPreview extends React.Component {
   render() {
 
     let entry = this.props.entry;
-    let data = entry.getIn(['data', 'it1']);
+    let it1 = entry.getIn(['data', 'it1']);
+    let it2 = entry.getIn(['data', 'it2']);
     let picture = entry.getIn(['data', 'picture']);
     let bg = this.props.getAsset(picture);
     console.log(bg)
@@ -13,7 +14,9 @@ export default class ItemsPreview extends React.Component {
       <div>
         Modified version
         <ul>
-          <img src={bg} />
+          <li>{it1}</li>
+          <li>{it2}</li>
+          <img height="200" width="200" src={bg} />
           
         </ul>
       </div>
